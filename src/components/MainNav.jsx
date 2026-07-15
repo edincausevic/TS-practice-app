60980a423e1a253ea2272a5b23b0c1547031b5bd
edincausevic <edincausevic.p@gmail.com>
2026-07-15 22:05:54 +0200

    before changing the app

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -38,10 +38,61 @@ const MainNav = ({displayExercise, data}) => {
 
             if(listItem.type === 'title') {
               return (
-                <div>
-                  <h2 key={listItem.id} style={{fontSize: '1.2rem', margin: '20px 0px 5px 10px'}}>{listItem.title}</h2>
-                  <div>
-                    <p>Stats: 3/42</p><button>Reset</button>
+                <div key={listItem.id} style={{marginBottom: '25px'}}>
+                  <h2  style={{fontSize: '1.2rem', margin: '20px 0px 5px 10px'}}>{listItem.title}</h2>
+                  <div className="header-stats flex" style={{marginLeft: '10px'}}>
+
+                    <div>
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="18" 
+                        height="18" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="2" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        style={{display: 'inline-block', color: '#b91c1c', marginRight: '4px'}}
+                      >
+                        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+                        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                        <line x1="12" y1="17" x2="12.01" y2="17"></line>
+                      </svg>
+                      <strong> 0 / </strong>
+                      <strong style={{color: '#b91c1c'}}>0 </strong>
+                      <strong>/ 112</strong>
+                    </div>
+
+                
+                    <div style={{margin: '0px 12px'}}></div>
+
+                
+                    <div>
+                      <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="18" 
+                        height="18" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="2" 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        style={{display: 'inline-block', marginRight: '4px', color: 'rgb(0, 92, 255)', position: 'relative', top: '-2px'}}
+                      >
+                        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                        <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                        <line x1="9" y1="12" x2="10" y2="13"></line>
+                        <line x1="15" y1="12" x2="15.01" y2="12"></line>
+                        <line x1="9" y1="16" x2="10" y2="17"></line>
+                        <line x1="15" y1="16" x2="15.01" y2="16"></line>
+                      </svg>
+                      <strong> 0 </strong>
+                      <strong>/ 23</strong>
+                    </div>
+
+                    <button style={{marginLeft: '25px', color: '#b91c1c', cursor: 'pointer'}}><span>↻</span> Reset</button>
                   </div>
                 </div>
               )
